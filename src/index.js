b6cacb7db68012573869fe77ab0869cf5ba519d1
Philip Stafford <philip.stafford@paddypowerbetfair.com>
2016-04-01 15:10:24 +0100

    Lesson 22 - Created a constructor to setup the state of the data that will be displayed in the app.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
-import React from 'react';
+//import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyDC1qRlI-6ZfrS2zl2iWwz3fiM7wXrAg0M'
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data);
-});
-
 //Create a new component - this will produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+//const App = () => {
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, function(videos) {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 //Take the component's generated HTML and put it on the page (in the DOM)
