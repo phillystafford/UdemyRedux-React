e12defa80ffadc9d76514687dc027ae4b5072787
Philip Stafford <philip.stafford@paddypowerbetfair.com>
2016-04-01 13:02:30 +0100

    Lesson 15 - Added an event listener and event handler to the SearchBar component. We started it in ES5 and then converted it to ES6

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
     render() {
-        return <input />;
+        //return <input onChange={this.onInputChange} />;
+        //return <input onChange={(event) => console.log(event.target.value)} />;
+        return <input onChange={event => console.log(event.target.value)} />;
     }
+
+    // this would be the old syntax but ES6 now allows us to do it in the <input /> tag
+    //onInputChange(event) {
+    //    console.log(event.target.value);
+    //    console.log(event); // doesn't work with the event property on its own
+    //
+    //}
 }
 
 export default SearchBar;
