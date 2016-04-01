e88290e1f62cad73f8b09470c81be3e17c799584
Philip Stafford <philip.stafford@paddypowerbetfair.com>
2016-04-01 14:19:11 +0100

    Lesson 17 - Added the constructor for the SearchBar and added some commented out lines of code to illustrate the difference between ES5 and ES6 Javascript

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,15 @@
+//import React from 'react';
 import React, { Component } from 'react';
 
+//class SearchBar extends React.Component {
 class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+
     render() {
         //return <input onChange={this.onInputChange} />;
         //return <input onChange={(event) => console.log(event.target.value)} />;
