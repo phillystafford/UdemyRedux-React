a8a5d55ba38703f364e41e05c8f7a5bd135bb8f0
Philip Stafford <philip.stafford@paddypowerbetfair.com>
2016-04-01 09:39:05 +0100

    Lesson 11 - Rendering the content on the screen. .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//Create a new component - this will produce HTML
-const App =  function(){
+//Create a new component - this will produce some HTML
+const App = () => {
     return <div>Hi!</div>;
 }
 
-//Take the component's generated HTML and put it in the DOM
-ReactDOM.render(App);
+//Take the component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
