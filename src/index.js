e17b90b33b89c844edc7230f6a6f6bbb34a15991
Philip Stafford <philip.stafford@paddypowerbetfair.com>
2016-04-01 11:39:56 +0100

    Lesson 14 - Created SearchBar component, imported it and rendered it on the screen.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyDC1qRlI-6ZfrS2zl2iWwz3fiM7wXrAg0M'
 
 //Create a new component - this will produce some HTML
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 //Take the component's generated HTML and put it on the page (in the DOM)
