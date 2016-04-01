91bf51f8274e325f9130e3bb39e66bde53779abd
Philip Stafford <philip.stafford@paddypowerbetfair.com>
2016-04-01 14:48:31 +0100

    Lesson 19 - Changed the SearchBar to update the state when the user alters the text, not vis versa.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -17,7 +17,9 @@ class SearchBar extends Component {
         //return <input onChange={event => console.log(event.target.value)} />;
         return (
             <div>
-                <input onChange={event => this.setState({ term: event.target.value })} />;
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({ term: event.target.value })} />
             </div>
         );
     }
