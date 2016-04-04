4ec00396a0b8e7ef22f48eebdc37b407fbd4f107
Philip Stafford <philip.stafford@paddypowerbetfair.com>
2016-04-04 12:28:22 +0100

    Lesson 31 - Updated the search feature to search when the user types inthe search bar. This includes callbacks.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -19,9 +19,14 @@ class SearchBar extends Component {
             <div className="search-bar">
                 <input
                     value={this.state.term}
-                    onChange={event => this.setState({ term: event.target.value })} />
+                    onChange={event => this.onInputChange(event.target.value)} />
             </div>
         );
+    }
+
+    onInputChange(term){
+        this.setState({term});
+        this.props.onSearchTermChange(term);
     }
 
     // this would be the old syntax but ES6 now allows us to do it in the <input /> tag
--- src/index.js
+++ src/index.js
@@ -19,7 +19,11 @@ class App extends Component {
             selectedVideo: null
         };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
             this.setState({
                 videos: videos,
                 selectedVideo: videos[0]
@@ -30,7 +34,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
                 <VideoDetail videos={this.state.selectedVideo} />
                 <VideoList
                     onVideoSelect={selectVideo => this.setState({selectedVideo}) }
