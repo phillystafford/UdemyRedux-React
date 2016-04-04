7a284d2eee35ae5944d2a37425ef9fca1623bfd0
Philip Stafford <philip.stafford@paddypowerbetfair.com>
2016-04-04 12:41:52 +0100

    Lesson 32 - Updated the search feature to search 300 milliseconds after the user finishes typing.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 //import React from 'react';
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -32,9 +33,10 @@ class App extends Component {
     }
 
     render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
         return (
             <div>
-                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+                <SearchBar onSearchTermChange={videoSearch} />
                 <VideoDetail videos={this.state.selectedVideo} />
                 <VideoList
                     onVideoSelect={selectVideo => this.setState({selectedVideo}) }
@@ -46,4 +48,3 @@ class App extends Component {
 
 //Take the component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
-
